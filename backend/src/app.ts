152b19ad8f9fe966930e63c390aa049626ac3410
Frosmin <simonabastomart@gmail.com>
2026-05-17 11:53:41 -0400

    2231313

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,10 @@
 import express, { type Application, type Request, type Response } from "express";
-import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
 import errorHandler from "./middlewares/error.middleware.js";
 
 const app: Application = express();
 
-// app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
